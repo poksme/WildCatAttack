708ff2f2226795c313207e08d2a74588815eba32
Linkinou <cedric.merouani@epitech.eu>
2014-05-22 11:52:36 +0900

    WIP : Double tap to Dash

--- WildCatProj/Assets/Scripts/Dash.cs
+++ WildCatProj/Assets/Scripts/Dash.cs
@@ -1,15 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Dash2 : MonoBehaviour {
+public class Dash : MonoBehaviour {
 
 	Rigidbody	RigidComp;
-	bool prout=false;
-	float	CountDown = 0f;
-	public const float dashTimerMax = 2f;
+	bool TimerActivated = false;
 	private float dashTimerCur = 0f;
 	GameObject Body;
 	public Rigidbody[] rigidBodies;
+	public const float dashTimerMax = 2f;
 
 	// Use this for initialization
 	void Start () {
@@ -19,27 +18,21 @@ public class Dash2 : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		bool test;
 		if (Input.GetKey("a")) {
-			//Vector3 target = new Vector3(transform.position.x + 5f, transform.position.y, transform.position.z);
-			//float step = 10 * Time.deltaTime;
-			//RigidComp.AddForce(Vector3.right * 1000);
-			//transform.position = 
-			//CountDown = 1f;
-			if (!prout) {
+			if (!TimerActivated) {
 				dashTimerCur = dashTimerMax;
-			DoDash(Vector3.left);
-				prout = true;
-			}
-		} else if (Input.GetKey("d")) {
-			if (!prout) {
-				dashTimerCur = dashTimerMax;
-				DoDash(Vector3.right);
-				prout = true;
+				DoDash(Vector3.left);
+				TimerActivated = true;
 			}
 		} else {
-			prout = false;
+			TimerActivated = false;
 		}
-		if (!prout) {
+		UpdateTimer();
+	}
+
+	void UpdateTimer() {
+		if (!TimerActivated) {
 			dashTimerCur += Time.deltaTime;
 		}
 		if (dashTimerCur > dashTimerMax) {
@@ -47,21 +40,12 @@ public class Dash2 : MonoBehaviour {
 				rb.velocity = Vector3.zero;
 			dashTimerCur = 0f;
 		}
-		if (CountDown >= 0f)
-		{
-			Debug.Log("COUOCU");
-			CountDown -= Time.deltaTime;
-			transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 0);
-		}
-		// if timer finished
-		// rigidbody.velocity = Vector3.zero;
-		    
 	}
 
 	void DoDash(Vector3 direction) {
 		rigidBodies = gameObject.GetComponentsInChildren<Rigidbody>();
 		foreach (Rigidbody rb in rigidBodies) {
-				rb.AddForce(direction * 15, ForceMode.Impulse);
+			rb.AddForce(direction * 15, ForceMode.Impulse);
 	}
 		// start timer
 		//this.animation.Play("Dash");
