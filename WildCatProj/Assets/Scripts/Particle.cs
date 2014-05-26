10605bb1230dfe5c21ce26ffc276e9b90879e3d1
Linkinou <cedric.merouani@epitech.eu>
2014-05-26 18:06:49 +0900

    Particle sandbox scene + Particles package

---
+++ WildCatProj/Assets/Scripts/Particle.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Particle : MonoBehaviour {
+
+	ParticleSystem		ParticleComp;
+
+	// Use this for initialization
+	void Start () {
+		ParticleComp = this.gameObject.GetComponent<ParticleSystem>();
+		ParticleComp.emissionRate = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if( Input.GetKeyDown( KeyCode.A ) ||  Input.GetKeyDown( KeyCode.D )) {
+			particleSystem.Emit(10);
+		}
+	}
+}
