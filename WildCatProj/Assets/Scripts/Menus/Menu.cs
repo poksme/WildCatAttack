835420beb852c94861d8d6f64ae4390e0b001171
freedonaab <freedonaab@gmail.com>
2014-05-28 17:55:19 -0700

    worked on menu scene: menuelement script + basic scene setup

---
+++ WildCatProj/Assets/Scripts/Menus/Menu.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+	public MenuElement[]	MenuElements;
+	public float			CameraSpeed = 3f;
+	public float			CameraDepth = 0.1f;
+
+	private int				TotalElements;
+	private int				CurrentElement;
+	private Vector3			CurrentCameraLookAt;
+
+	private void Start () {
+		this.TotalElements = this.MenuElements.Length;
+		this.CurrentElement = 0;
+	}
+	
+	private void Update () {
+		int oldCurrentElement = this.CurrentElement;
+		bool dirty = false;
+		if (Input.GetKeyDown("d")) {
+			if (this.CurrentElement == (this.TotalElements - 1))
+				this.CurrentElement = 0;
+			else
+				this.CurrentElement += 1;
+			dirty = true;
+		}
+		else if (Input.GetKeyDown("a")) {
+			if (this.CurrentElement == 0)
+				this.CurrentElement = (this.TotalElements - 1);
+			else
+				this.CurrentElement -= 1;
+			dirty = true;
+		}
+		//selection/deselection
+		if (dirty) {
+			this.MenuElements[oldCurrentElement].Select();
+			this.MenuElements[this.CurrentElement].UnSelect();
+		}
+
+		
+		//Camera movement
+		Camera.main.transform.position = Vector3.Lerp(Camera.main.transform.position, this.MenuElements[this.CurrentElement].CameraPosition.position, CameraSpeed * Time.deltaTime);
+		this.CurrentCameraLookAt = Vector3.Lerp(this.CurrentCameraLookAt, this.MenuElements[this.CurrentElement].CameraLookAt.position, CameraSpeed * Time.deltaTime);
+		Vector3 pos = Camera.main.transform.position;
+		pos.y += CameraDepth;
+		Camera.main.transform.position = pos;
+		Camera.main.transform.LookAt(this.CurrentCameraLookAt);
+	}
+}
---
+++ WildCatProj/Assets/Scripts/Menus/MenuElement.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class MenuElement : MonoBehaviour {
+
+	//public attributes
+	public	Transform	CameraLookAt;
+	public	Transform	CameraPosition;
+
+	public event	Action	OnMenuAction;
+	public event	Action	OnSelect;
+	public event	Action	OnUnSelect;
+
+	//public properties
+	public	bool		Selected { get; private set; }
+
+
+	//public method
+	public	void		Select() {
+		this.Selected = true;
+		this.OnSelect();
+	}
+
+	public	void		UnSelect() {
+		this.Selected = false;
+		this.OnUnSelect();
+	}
+
+	public void			MenuAction() {
+		this.MenuAction();
+	}
+
+	//private Unity Methods
+	private	void		Awake() {
+		this.OnMenuAction += empty;
+		this.OnSelect += empty;
+		this.OnUnSelect += empty;
+	}
+
+	private void		empty() {
+
+	}
+}
