58177773840d6f71faa5461a3996e851376910e7
pachoulie <souy.angelina@gmail.com>
2014-06-05 15:31:51 +0900

    Enhanced Background Element

---
+++ WildCatProj/Assets/Scripts/ConveyorBeltBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConveyorBeltBehaviour : MonoBehaviour {
+
+	public	bool	reverseDirection = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnCollisionStay(Collision collision) {
+		if (reverseDirection == true)
+			collision.gameObject.GetComponentInChildren<Rigidbody>().velocity = Vector3.left * 5;
+		else
+			collision.gameObject.GetComponentInChildren<Rigidbody>().velocity = Vector3.right * 5;
+	}
+}
---
+++ WildCatProj/Assets/Scripts/SpawnPoint.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SpawnPoint : MonoBehaviour {
+
+	public	List<GameObject> BrokenWildCats;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("SpawnObject", 0, 5);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void SpawnObject() {
+		Instantiate(BrokenWildCats[(Random.Range(0, BrokenWildCats.Count))], this.transform.position, Random.rotation);
+	}
+}
