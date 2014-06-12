62d08da91c06699d1b41f418a25f1093f085bfae
Linkinou <cedric.merouani@epitech.eu>
2014-06-12 11:18:27 +0900

    New scenes

---
+++ WildCatProj/Assets/Scripts/GameModes.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameModes : MonoBehaviour {
+
+	OptionManager options; 
+	public GameObject	floor;
+
+	// Use this for initialization
+	void Start () {
+		options = OptionManager.GetInstance();
+
+		if (options.snowModeActivated) {
+			floor.collider.material = (PhysicMaterial)Resources.Load("PhysicMaterials/Ice");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
